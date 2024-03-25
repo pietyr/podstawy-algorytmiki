367f2b7cb1911968c6e48d88b76df1d9e310c6f4
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-03-25 18:31:58 +0100

    feat: copy utility functions from Lab1

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -39,9 +39,69 @@ void insertSort(int *array, int size, int mode = 0);
  * @param mode 0 (default) - ascending, 1 - descending
  * @param column index of column (array [][column])
  */
-void bubbleSort2D(int **array, int sizeX, int sizeY, int mode = 0, int column);
+void bubbleSort2D(int **array, int sizeX, int sizeY, int column, int mode = 0);
 
-int main()
-{
-    return 0;
+/**
+ * Allocate memory for one dimension array
+ * @param array Reference to the array
+ * @param size Size of the array
+ */
+void allocateMemory1D(int *&array, int size);
+
+/**
+ * Fill the one dimension array with numbers from <a,b> range
+ * @param array Reference to target array
+ * @param size Size of the target array
+ * @param min Lowest fill value (including)
+ * @param max Highest fill value (including)
+ */
+void randomFill1DArray(int *array, int size, int min, int max);
+
+/**
+ * Free memory of the one dimension array
+ * @param array Target array
+ */
+void deleteArray1D(int *&array);
+
+/**
+ * Print content of the one dimension array
+ * @param array Target array
+ * @param size Size of the array
+ */
+void print1D(int* array, int size);
+
+int main(){
+	return 0;
+}
+
+void bubbleSort(int *array, int size, int mode){
+	cout << "test";
+}
+
+void allocateMemory1D(int *&array, int size) {
+	array = new int[size];
+}
+
+void randomFill1DArray(int *array, int size, int min, int max) {
+	for(int i = 0; i < size; i++) {
+		int randomNumber = rand() % (max - min + 1) + min;
+		array[i] = randomNumber;
+	}
+}
+
+void deleteArray1D(int *&array) {
+	delete []array;
+}
+
+void print1D(int* array, int size) {
+	cout << "[";
+
+	for(int i = 0; i < size; i++) {
+		cout << array[i];
+		if(i < size - 1) {
+			cout << ", ";
+		}
+	}
+
+	cout << "]" << endl;
 }
