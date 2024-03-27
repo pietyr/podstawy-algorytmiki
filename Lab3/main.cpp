af6639818d08d57f97a967e9b776706a9d9f4aa7
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-03-27 16:44:17 +0100

    feat: init Lab3 project; fill it with functions prototypes

---
+++ Lab3/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+struct student {
+	// TODO
+};
+
+/**
+ * Function sorts array of students (by points) with quick sort algorithm
+ * @param array
+ * @param size
+ * @param mode 0 - ascending sorting, other number - descending sorting
+ */
+void quickSort(student *&array, int size, int mode);
+
+/**
+ * Allocate array memory
+ * @param array
+ * @param size
+ */
+void loadStudents(student *&array, int size);
+
+/**
+ * Delete array from memory
+ * @param array
+ */
+void deleteStudentsArray(student *&array);
+
+/**
+ * Print content of students array
+ * @param array
+ * @param size
+ */
+void printStudents(student *&array, int size);
+
+int main() {
+	return 0;
+}
