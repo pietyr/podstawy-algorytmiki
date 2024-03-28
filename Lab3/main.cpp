9ad941c01d9eab68098477830ce35d6f020ee578
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2024-03-28 17:11:21 +0100

    feat: add quickSort

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -11,12 +11,25 @@ struct student {
 };
 
 /**
+ * Rearange array (Find the partition point) for quickSort
+ * @param array
+ * @param low
+ * @param high
+ * @param size
+ * @param mode
+ * @return
+ */
+int partition(student *&array, int *size, int mode, int low, int high);
+
+/**
  * Function sorts array of students (by score) with quick sort algorithm
  * @param array
+ * @param low
+ * @param high
  * @param size
  * @param mode 0 - ascending sorting, other number - descending sorting
  */
-void quickSort(student *&array, int size, int mode);
+void quickSort(student *&array, int *size, int mode, int low, int high);
 
 /**
  * Allocate array memory
@@ -43,8 +56,43 @@ int main() {
 	int *size = new int;
 	loadStudents(array, size);
 	printStudents(array, size);
+	quickSort(array, size, 0, 0, *size - 1);
+	printStudents(array, size);
 	delete size;
 	return 0;
+}
+
+void swap(student *a, student *b) {
+	student temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+int partition(student *&array, int *size, int mode, int low, int high) {
+	int pivot = array[high].score;
+	int index = low - 1;
+
+	// Compare every element of the array with pivot
+	for(int i = low; i < high; i++) {
+		// Swap if element is smaller than pivot
+		if(array[i].score <= pivot) {
+			swap(array[++index], array[i]);
+		}
+	}
+
+	swap(array[index + 1], array[high]);
+
+	return index + 1;
+}
+
+void quickSort(student *&array, int *size, int mode, int low, int high) {
+	if(low < high) {
+		int pivot = partition(array, size, mode, low, high);
+
+		quickSort(array, size, mode, low, pivot - 1);
+		quickSort(array, size, mode, pivot + 1, high);
+
+	}
 }
 
 void loadStudents(student *&array, int *size) {
@@ -76,6 +124,9 @@ void loadStudents(student *&array, int *size) {
 	file.close();
 }
 
+void deleteStudentsArray(student *&array) {
+	delete []array;
+}
 
 void printStudents(student *&array, int *size) {
 	cout << "Imię Nazwisko - Liczba punktów" << endl;
